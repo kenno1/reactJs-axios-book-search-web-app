26c35d3d2292d48bc0d003b17914b9c50fb87324
kambe.kenta <kenta.k199405@gmail.com>
2021-02-12 10:03:25 +0900

    added search books function.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import {
   Label,
   Spinner
 } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import axios from 'axios';
 
 function App() {
   const [maxResults, setMaxResults] = useState(10);
@@ -16,6 +19,34 @@ function App() {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [cards, setCards] = useState([]);
+  
+  const handleSubmit = () => {
+    setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error('max results must be between 1 and 40');
+    } else {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+        )
+        .then(res => {
+          if (startIndex >= res.data.totalItems || startIndex < 1) {
+            toast.error(
+              `max reults must be between 1 and ${res.data.totalItems}`
+            );
+          } else {
+            if (res.data.items.length > 0) {
+              setCards(res.data.items);
+              setLoading(false);
+            }
+          }
+        })
+        .catch(err => {
+          setLoading(true);
+          console.log(err.response);
+        });
+    }
+  };
   
   const mainHeader = () => {
     return (
@@ -58,8 +89,9 @@ function App() {
     );
   }
   return (
-    <div>
+    <div className='w-100 h-100'>
       {mainHeader()}
+      <ToastContainer />
     </div>
   );
 }
