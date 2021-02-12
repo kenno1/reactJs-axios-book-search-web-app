66a04f85108ff68f73749e5bf99a5afe2e212673
kambe.kenta <kenta.k199405@gmail.com>
2021-02-12 10:13:38 +0900

    added Bookcard template.

--- src/App.jsx
+++ src/App.jsx
@@ -98,6 +98,45 @@ function App() {
       </div>
     );
   };
+
+  const handleCards = () => {
+    if (loading) {
+      return (
+        <div className='d-flex justify-content-center mt-3'>
+          <Spinner style={{ width: '3rem', height: '3rem' }} />
+        </div>
+      );
+    } else {
+      const items = cards.map((item, i) => {
+        let thumbnail = '';
+        if (item.volumeInfo.imageLinks) {
+          thumbnail = item.volumeInfo.imageLinks.thumbnail;
+        }
+
+        return (
+          <div className='col-lg-4 mb-3' key={item.id}>
+            <BookCard
+              thumbnail={thumbnail}
+              title={item.volumeInfo.title}
+              pageCount={item.volumeInfo.pageCount}
+              language={item.volumeInfo.language}
+              authors={item.volumeInfo.authors}
+              publisher={item.volumeInfo.publisher}
+              description={item.volumeInfo.description}
+              previewLink={item.volumeInfo.previewLink}
+              infoLink={item.volumeInfo.infoLink}
+            />
+          </div>
+        );
+      });
+      return (
+        <div className='container my-5'>
+          <div className='row'>{items}</div>
+        </div>
+      );
+    }
+  };
+  
   return (
     <div className='w-100 h-100'>
       {mainHeader()}
