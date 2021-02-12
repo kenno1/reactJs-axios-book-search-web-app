09f1cd557755b7824b9bc116c6fd32199e350a1c
kambe.kenta <kenta.k199405@gmail.com>
2021-02-12 10:09:14 +0900

    added functions.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   const handleSubmit = () => {
     setLoading(true);
     if (maxResults > 40 || maxResults < 1) {
-      toast.error('max results must be between 1 and 40');
+      toast.error('max results must be between 1 and 40.');
     } else {
       axios
         .get(
@@ -32,7 +32,7 @@ function App() {
         .then(res => {
           if (startIndex >= res.data.totalItems || startIndex < 1) {
             toast.error(
-              `max reults must be between 1 and ${res.data.totalItems}`
+              `max results must be between 1 and ${res.data.totalItems}.`
             );
           } else {
             if (res.data.items.length > 0) {
@@ -47,21 +47,27 @@ function App() {
         });
     }
   };
-  
+
   const mainHeader = () => {
     return (
-      <div className='main-image d-flex jsutify-content-center align-items-center flex-column'>
-        <div className="filter"></div>
-        <h1 className='display-2 text-center text-white mb-3' style={{ zIndex: 2 }}>
-          Google Book Search
+      <div className='main-image d-flex justify-content-center align-items-center flex-column'>
+        {/* Overlay */}
+        <div className='filter'></div>
+        <h1
+          className='display-2 text-center text-white mb-3'
+          style={{ zIndex: 2 }}
+        >
+          Google Books
         </h1>
         <div style={{ width: '60%', zIndex: 2 }}>
           <InputGroup size='lg' className='mb-3'>
             <Input
               placeholder='Book Search'
+              value={query}
+              onChange={e => setQuery(e.target.value)}
             />
             <InputGroupAddon addonType='append'>
-              <Button color='secondary'>
+              <Button color='secondary' onClick={handleSubmit}>
                 <i className='fas fa-search'></i>
               </Button>
             </InputGroupAddon>
@@ -73,7 +79,9 @@ function App() {
                 type='number'
                 id='maxResults'
                 placeholder='Max Results'
-                />
+                value={maxResults}
+                onChange={e => setMaxResults(e.target.value)}
+              />
             </FormGroup>
             <FormGroup className='ml-5'>
               <Label for='startIndex'>Start Index</Label>
@@ -81,13 +89,15 @@ function App() {
                 type='number'
                 id='startIndex'
                 placeholder='Start Index'
-                />
+                value={startIndex}
+                onChange={e => setStartIndex(e.target.value)}
+              />
             </FormGroup>
           </div>
         </div>
       </div>
     );
-  }
+  };
   return (
     <div className='w-100 h-100'>
       {mainHeader()}
